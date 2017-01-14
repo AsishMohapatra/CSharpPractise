046d7e63cc3506a3ae194f30598949de77a6033b
AsishMohapatra <mohapatra.asish@gmail.com>
2017-01-15 00:23:18 +0800

    HttpGetWithNoParam

--- WebApiPractise/WebApiPractise/Program.cs
+++ WebApiPractise/WebApiPractise/Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebApiPractise
 {
     class Program
     {
+        static string downloadedData;
         static void Main(string[] args)
         {
+            Console.WriteLine("Request For DownLoad start");
+            var downloadedDataLength=WebsiteContentDownLoader.GetUrlContentAsync("http://www.google.com", "");
+            Console.WriteLine("Download task status = " + downloadedDataLength.Status);
+            Console.WriteLine("Download task status = " + 
+                (downloadedDataLength.IsCompleted?"Completed": "In Progress"));
+            Thread.Sleep(20000);
+            Console.WriteLine("Total length = " + downloadedDataLength.Result);
+            Console.ReadLine();
         }
     }
 }
--- WebApiPractise/WebApiPractise/WebsiteContentDownLoader.cs
+++ WebApiPractise/WebApiPractise/WebsiteContentDownLoader.cs
@@ -4,17 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace WebApiPractise
 {
     class WebsiteContentDownLoader
     {
-        public static string  GetContent(string baseurl,string requesturi)
+        public async static Task<int>  GetUrlContentLengthAsync(string baseurl,string requesturi)
         {
             HttpClient webclient = new HttpClient();
             webclient.BaseAddress = new Uri(baseurl);
-            Task<string> webContent=webclient.GetStringAsync(requesturi);
+            var downloadingTaskToTrack= webclient.GetStringAsync(requesturi);
+            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id"+downloadingTaskToTrack.Id+
+                "   and TasState as"  + downloadingTaskToTrack.AsyncState);
+            var downloadedData= await downloadingTaskToTrack;
+            return downloadedData.Length;
+        }
 
+        public async static Task<string> GetUrlContentAsync(string baseurl, string requesturi)
+        {
+            HttpClient webclient = new HttpClient();
+            webclient.BaseAddress = new Uri(baseurl);
+            var downloadingTaskToTrack = webclient.GetStringAsync(requesturi);
+            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id" + downloadingTaskToTrack.Id +
+                "   and TasState as" + downloadingTaskToTrack.AsyncState);
+            var downloadedData = await downloadingTaskToTrack;
+            return downloadedData;
+        }
+
+        public async static Task<T> GetUrlContentJsonAsync<T>(string baseurl, string requesturi)
+        {
+            HttpClient webclient = new HttpClient();
+            webclient.BaseAddress = new Uri(baseurl);
+            var downloadingTaskToTrack = webclient.GetStringAsync(requesturi);
+            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id" + downloadingTaskToTrack.Id +
+                "   and TasState as" + downloadingTaskToTrack.AsyncState);
+            var downloadedData = await downloadingTaskToTrack;
+            var jsonOutput = JsonConvert.DeserializeObject<T>(downloadedData);
+            return jsonOutput;
         }
     }
 }
