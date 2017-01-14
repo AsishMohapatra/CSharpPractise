95af2b345c6d1163105b9662eae3052892efffbe
AsishMohapatra <mohapatra.asish@gmail.com>
2017-01-14 20:31:05 +0800

    CsharpConcepts
    C# concepts practise

---
+++ WebApiPractise/WebApiPractise/WebsiteContentDownLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WebApiPractise
+{
+    class WebsiteContentDownLoader
+    {
+        public static string  GetContent(string baseurl,string requesturi)
+        {
+            HttpClient webclient = new HttpClient();
+            webclient.BaseAddress = new Uri(baseurl);
+            Task<string> webContent=webclient.GetStringAsync(requesturi);
+
+        }
+    }
+}
