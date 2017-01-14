2865b50807f9c16eaca2bb1fbdaf17c62462cd42
AsishMohapatra <mohapatra.asish@gmail.com>
2017-01-15 01:38:49 +0800

    Revert "HttpGetWithNoParam"
    This reverts commit 046d7e63cc3506a3ae194f30598949de77a6033b.
    
    # Conflicts:
    #	WebApiPractise/WebApiPractise.v12.suo

--- AsyncWPFSample/AsyncWPFSample/Program.cs
+++ AsyncWPFSample/AsyncWPFSample/Program.cs
@@ -10,6 +10,7 @@ namespace AsyncWPFSample
     {
         static void Main(string[] args)
         {
+            //
         }
     }
 }
--- WebApiPractise/WebApiPractise/WebsiteContentDownLoader.cs
+++ WebApiPractise/WebApiPractise/WebsiteContentDownLoader.cs
@@ -4,44 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
-using Newtonsoft.Json;
 
 namespace WebApiPractise
 {
     class WebsiteContentDownLoader
     {
-        public async static Task<int>  GetUrlContentLengthAsync(string baseurl,string requesturi)
+        public static string  GetContent(string baseurl,string requesturi)
         {
             HttpClient webclient = new HttpClient();
             webclient.BaseAddress = new Uri(baseurl);
-            var downloadingTaskToTrack= webclient.GetStringAsync(requesturi);
-            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id"+downloadingTaskToTrack.Id+
-                "   and TasState as"  + downloadingTaskToTrack.AsyncState);
-            var downloadedData= await downloadingTaskToTrack;
-            return downloadedData.Length;
-        }
+            Task<string> webContent=webclient.GetStringAsync(requesturi);
 
-        public async static Task<string> GetUrlContentAsync(string baseurl, string requesturi)
-        {
-            HttpClient webclient = new HttpClient();
-            webclient.BaseAddress = new Uri(baseurl);
-            var downloadingTaskToTrack = webclient.GetStringAsync(requesturi);
-            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id" + downloadingTaskToTrack.Id +
-                "   and TasState as" + downloadingTaskToTrack.AsyncState);
-            var downloadedData = await downloadingTaskToTrack;
-            return downloadedData;
-        }
-
-        public async static Task<T> GetUrlContentJsonAsync<T>(string baseurl, string requesturi)
-        {
-            HttpClient webclient = new HttpClient();
-            webclient.BaseAddress = new Uri(baseurl);
-            var downloadingTaskToTrack = webclient.GetStringAsync(requesturi);
-            Console.WriteLine("DownLoad Not Yet Started but wee have a task Id" + downloadingTaskToTrack.Id +
-                "   and TasState as" + downloadingTaskToTrack.AsyncState);
-            var downloadedData = await downloadingTaskToTrack;
-            var jsonOutput = JsonConvert.DeserializeObject<T>(downloadedData);
-            return jsonOutput;
         }
     }
 }
